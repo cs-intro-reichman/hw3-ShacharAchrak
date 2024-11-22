9e04c7a8bc519bd7d60e3983be4f0d75f24e3e6a
ShacharAchrak <shachar.achrak@post.runi.ac.il>
2024-11-22 20:10:23 +0200

    Algebra

--- Algebra.java
+++ Algebra.java
@@ -25,43 +25,112 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int sum = x1;
+		if(x1==0){
+			return x2;
+		
+		}   else{
+			    if(x2==0){
+			    return x1;
+
+		}   else{
+		    	for(int i=0; i<x2; i++){
+				sum++;
+			}
+		  }
+		}
+		return sum;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int difference = x1;
+			    if(x2==0){
+			    return x1;
+
+		}   else{
+		    	for(int i=0; i<x2; i++){
+					difference--;
+			}
+		  }
+		
+		return difference;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int product = 0;
+		if(x1==0 || x2 ==0){
+			return 0;
+		
+		}else{
+		    	for(int i=0; i<x2; i++){
+					product = plus(product, x1);
+			}
+		  }
+		
+		return product;
 	}
-
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int power = 1;
+		if(x==0){
+			return 0;
+		
+		}   else{
+			    if(n==0){
+			    return 1;
+
+		}   else{
+		    	for(int i=0; i<n; i++){
+				power = times(power, x);
+			}
+		  }
+		}
+		return power;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int quotient = 0;
+		int remainig =x1;
+		if(x1 == 0 || x2 == 0){
+			return 0;
+		
+		}else{
+		    	while(remainig >= x2){
+					remainig = minus(remainig, x2);
+					quotient ++ ;
+				}
+		}
+		
+		return quotient;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}	
+		int remainig = x1;
+		if(x1 == 0 || x2 == 0){
+			return 0;
+		
+		}else{
+		    	while(remainig >= x2){
+					remainig = minus(remainig, x2);
+				}
+				return remainig;
+		}
+		
+	}
+	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
-	}	  	  
+		int result = 0;
+		int increment = 1;
+		    while (times(result, result) <= x) {
+			    result = plus(result, increment);
+			} 
+
+			 return minus(result,1);
+    }	  	  
 }
